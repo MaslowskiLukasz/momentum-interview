09ad14188d0b496da1725c31aefd664e3ffa10a7
MaslowskiLukasz <l.maslowski2012@gmail.com>
2025-03-30 20:09:36 +0200

    add team stats and stats tile components

---
+++ components/TeamStats.vue
@@ -0,0 +1,14 @@
+<script setup>
+const { team } = defineProps(['team']);
+</script>
+
+<template>
+  <div>
+    <h3 class="text-xl font-semibold mb-4 dark:text-white">Team Stats</h3>
+    <div class="mt-6 grid grid-cols-1 md:grid-cols-3 gap-4">
+      <StatsTile label="Points" :value="team.points" />
+      <StatsTile label="Goals Scored" :value="team.goalsFor" />
+      <StatsTile label="Goals Conceded" :value="team.goalsAgainst" />
+    </div>
+  </div>
+</template>
