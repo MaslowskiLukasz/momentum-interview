d111080d877f87b20072b8d171dbaeaa45168d74
MaslowskiLukasz <l.maslowski2012@gmail.com>
2025-03-30 23:45:08 +0200

    add recent matches component

--- components/Details.vue
+++ components/Details.vue
@@ -241,22 +241,7 @@ function saveTeamDetails() {
         <!-- Matches List -->
         <div v-else>
           <!-- Recent Form Display -->
-          <div class="mb-6">
-            <h4 class="text-lg font-medium mb-2 dark:text-white">
-              Recent Form
-            </h4>
-            <div class="flex space-x-3">
-              <MatchResult
-                v-for="(result, index) in selectedTeam.recentForm"
-                :key="index"
-                :result="result"
-                :size="10"
-              />
-            </div>
-            <p class="mt-2 text-xs text-gray-500 dark:text-gray-400">
-              Most recent match on the right
-            </p>
-          </div>
+          <RecentForm :matches="selectedTeam.recentForm" />
 
           <!-- Match Statistics Summary -->
           <div class="mb-6 bg-gray-50 dark:bg-gray-700 p-4 rounded-lg">
