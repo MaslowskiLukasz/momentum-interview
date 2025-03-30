0a050b3dfcfffb139b87b68f6535ffaef1e22cc8
MaslowskiLukasz <l.maslowski2012@gmail.com>
2025-03-30 18:45:57 +0200

    add team details header

---
+++ components/TeamDetailsHeader.vue
@@ -0,0 +1,14 @@
+<script setup>
+const store = useTeamsStore();
+const { selectedTeam } = storeToRefs(store);
+</script>
+
+<template>
+  <div class="bg-gradient-to-r from-blue-500 to-blue-700 p-6 text-white">
+    <h2 class="text-3xl font-bold">{{ selectedTeam.name }}</h2>
+    <p class="text-blue-100">
+      Position: #{{ selectedTeam.position }} • Points:
+      {{ selectedTeam.points }}
+    </p>
+  </div>
+</template>
