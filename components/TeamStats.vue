9871ba9155c60a7b5bb0db0f7e44aa170810a1e0
MaslowskiLukasz <l.maslowski2012@gmail.com>
2025-03-30 22:39:46 +0200

    change tile component name

--- components/TeamStats.vue
+++ components/TeamStats.vue
@@ -6,9 +6,9 @@ const { team } = defineProps(['team']);
   <div>
     <h3 class="text-xl font-semibold mb-4 dark:text-white">Team Stats</h3>
     <div class="mt-6 grid grid-cols-1 md:grid-cols-3 gap-4">
-      <StatsTile label="Points" :value="team.points" />
-      <StatsTile label="Goals Scored" :value="team.goalsFor" />
-      <StatsTile label="Goals Conceded" :value="team.goalsAgainst" />
+      <Tile label="Points" :value="team.points" />
+      <Tile label="Goals Scored" :value="team.goalsFor" />
+      <Tile label="Goals Conceded" :value="team.goalsAgainst" />
     </div>
   </div>
 </template>
