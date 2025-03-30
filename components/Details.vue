6d0398894e9ba287e745d6cee956c5293e2827c2
MaslowskiLukasz <l.maslowski2012@gmail.com>
2025-03-30 23:25:22 +0200

    add success banner

--- components/Details.vue
+++ components/Details.vue
@@ -224,27 +224,7 @@ function saveTeamDetails() {
         <h3 class="text-xl font-semibold mb-4 dark:text-white">All Matches</h3>
 
         <!-- Success Message -->
-        <div
-          v-if="showEditSuccess"
-          class="mb-4 p-3 bg-green-100 text-green-800 dark:bg-green-900 dark:text-green-200 rounded-md flex items-center"
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            class="h-5 w-5 mr-2"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M5 13l4 4L19 7"
-            />
-          </svg>
-          Match result updated successfully! Standings have been recalculated.
-        </div>
-
+        <SuccessBanner v-if="showEditSuccess" />
         <!-- Loading State -->
         <div
           v-if="isLoadingMatches"
