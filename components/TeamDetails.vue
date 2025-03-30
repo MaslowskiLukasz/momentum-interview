d69dfe408bab27f08cd376da9a728fc6fa7941ae
MaslowskiLukasz <l.maslowski2012@gmail.com>
2025-03-30 20:27:57 +0200

    add icon components

--- components/TeamDetails.vue
+++ components/TeamDetails.vue
@@ -14,39 +14,13 @@ const emit = defineEmits(['editTeamInfo']);
 
     <div class="space-y-3">
       <div class="flex items-center space-x-2">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          class="h-5 w-5 text-gray-500 dark:text-gray-400"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
-          />
-        </svg>
+        <Calendar />
         <p class="dark:text-gray-300">
           <strong>Founded:</strong> {{ team.founded }}
         </p>
       </div>
       <div class="flex items-center space-x-2">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          class="h-5 w-5 text-gray-500 dark:text-gray-400"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M19 21V5a2 2 0 00-2-2H7a2 2 0 00-2 2v16m14 0h2m-2 0h-5m-9 0H3m2 0h5M9 7h1m-1 4h1m4-4h1m-1 4h1m-5 10v-5a1 1 0 011-1h2a1 1 0 011 1v5m-4 0h4"
-          />
-        </svg>
+        <Building />
         <p class="dark:text-gray-300">
           <strong>Stadium:</strong> {{ team.stadium }}
           <button
@@ -58,20 +32,7 @@ const emit = defineEmits(['editTeamInfo']);
         </p>
       </div>
       <div class="flex items-center space-x-2">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          class="h-5 w-5 text-gray-500 dark:text-gray-400"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
-          />
-        </svg>
+        <User />
         <p class="dark:text-gray-300">
           <strong>Coach:</strong> {{ team.coach }}
         </p>
