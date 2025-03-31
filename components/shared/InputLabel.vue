2c4f7584fefd03845493cdb9b6229bbe7c06c2ec
MaslowskiLukasz <l.maslowski2012@gmail.com>
2025-03-31 18:16:48 +0200

    add input label

---
+++ components/shared/InputLabel.vue
@@ -0,0 +1,10 @@
+<script setup>
+const { value } = defineProps(['value']);
+</script>
+
+<template>
+  <label
+    class="block text-sm font-medium text-gray-700 dark:text-gray-300 mb-1"
+    >{{ value }}</label
+  >
+</template>
